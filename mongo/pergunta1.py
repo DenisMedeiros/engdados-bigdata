286b7178bca32ea310a00b4b836c0fa19cacaca2
Mateus Targino <mateus@debian.com>
2018-06-21 01:30:47 -0300

    teste com spark

--- mongo/pergunta1.py
+++ mongo/pergunta1.py
@@ -1,5 +1,9 @@
 from pyspark.sql import SparkSession
 
+#configuração do spark
+SparkSession.builder.config('spark.jars.packages','org.mongodb.spark:mongo-spark-connector_2.11:2.2.0' ).getOrCreate()
+
+#conexão com a collection de candidatos
 spark_candidatos = SparkSession \
     .builder \
     .appName("myApp") \
@@ -7,6 +11,7 @@ spark_candidatos = SparkSession \
     .config("spark.mongodb.output.uri", "mongodb://10.7.40.54/eleicoes.candidatos2014") \
     .getOrCreate()
 
+#conexão com a collection de resultados
 spark_resultados = SparkSession \
     .builder \
     .appName("myApp") \
@@ -14,5 +19,6 @@ spark_resultados = SparkSession \
     .config("spark.mongodb.output.uri", "mongodb://10.7.40.54/eleicoes.resultados2014") \
     .getOrCreate()
 
+#leitura dos dados dos candidatos
 df_candidatos = spark_candidatos.read.format("com.mongodb.spark.sql.DefaultSource").load()
 df_candidatos.printSchema()
