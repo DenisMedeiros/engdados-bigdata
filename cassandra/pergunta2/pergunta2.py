6f6d1894d9f1b56a4c4ad06bcb8b6a23194d4da4
Denis <dnsricardo@gmail.com>
2018-06-22 02:05:18 -0300

    Pergunta 2 pronta, mas faltando testar.

--- cassandra/pergunta2/pergunta2.py
+++ cassandra/pergunta2/pergunta2.py
@@ -25,27 +25,26 @@ candidatosDF = spark.read.format("org.apache.spark.sql.cassandra").\
 # Obtém apenas as informações úteis.
 informacoes = candidatosDF.select('sequencial_candidato', 'sigla_partido')
 
-# Faz o join dos eleitos e das informações (partido).
-dados = codigos.join(informacoes, codigos['sq_candidato'] == informacoes['sequencial_candidato'], 'inner')
-
 # Gera um DataFrame com os dados dos pibs.
 pibsDF = spark.read.format("org.apache.spark.sql.cassandra").\
     options(keyspace="eleicoes", table="pibs").\
     load()
 
-# Obtém os PIBS de 2011.
+# Obtém os PIBS de acima de R$ 70.000,00;
 pibs = pibsDF.\
-    filter(pibsDF['ano'] == 2011).\
-    select('municipio', 'valor').\
-    distinct()
+    filter(pibsDF['pib_percapita'] > 700000.0).\
+    select('uf', 'cidade', 'cod_tse')
+
+# Faz o join dos eleitos e das informações (partido).
+dados_eleitos = codigos.join(informacoes, codigos['sq_candidato'] == informacoes['sequencial_candidato'], 'inner')
 
 # Faz o join dos eleitos e das informações.
-dados = codigos.join(pibs, codigos['codigo_municipio'] == pibs['municipio'], 'inner')
+final = dados_eleitos.join(pibs, dados_eleitos['codigo_municipio'] == pibs['cod_tse'], 'inner')
 
 # Obtém as respostas desejadas (conta as entradas para cada coluna).
-res_sexo = dados.filter(dados['valor'] > )
+res_partidos = final.groupby(['sigla_partido']).count()
 
 # Armazena o resultado no HDFS.
-res_sexo.write.format("csv").save("hdfs://dricardo-master:9000/user/engdados/res_sexo.csv")
+res_partidos.write.format("csv").save("hdfs://dricardo-master:9000/user/engdados/res_partidos.csv")
 
 print 'Encerrado com sucesso.'
