017d7815142b4f3e2fd6c2e24631f4550ccf971a
Mateus Targino <mateus@debian.com>
2018-06-22 19:20:15 -0300

    Mongo: pergunta 3

---
+++ mongo/pergunta3/pergunta3.py
@@ -0,0 +1,90 @@
+from pyspark.sql import SparkSession as spark
+
+#configuração do spark
+spark.builder.config('spark.jars.packages','org.mongodb.spark:mongo-spark-connector_2.11:2.2.0' ).getOrCreate()
+
+#ELEITORADO
+#conexão com a collection
+spark_eleitores = spark \
+    .builder \
+    .appName("eleitoresApp") \
+    .config("spark.mongodb.input.uri", "mongodb://10.7.40.54/eleicoes.eleitores") \
+    .config("spark.mongodb.output.uri", "mongodb://10.7.40.54/eleicoes.eleitores") \
+    .getOrCreate()
+
+df_eleitores = spark_eleitores.read.format("com.mongodb.spark.sql.DefaultSource").load()
+
+#seleciona informações úteis
+df_informacoes_eleitores = df_eleitores.select('cod_municipio_tse', 'grau_de_escolaridade', 'qtd_eleitores_no_perfil')
+
+#obtém total de eleitores
+total_eleitores = df_eleitores.groupby(['cod_municipio_tse']).sum('qtd_eleitores_no_perfil').withColumnRenamed('sum(qtd_eleitores_no_perfil)', 'total')
+
+#obtém eleitores com superior completo e baixa escolaridade
+df_eleitores_superior_completo = df_informacoes_eleitores.filter(df_informacoes_eleitores['grau_de_escolaridade'] == 'SUPERIOR COMPLETO')
+df_eleitores_baixa_esc = df_informacoes_eleitores.filter((df_informacoes_eleitores['grau_de_escolaridade'] == 'ANALFABETO' )| \
+ (df_informacoes_eleitores['grau_de_escolaridade'] == 'LÊ E ESCREVE') | \
+ (df_informacoes_eleitores['grau_de_escolaridade'] == 'ENSINO FUNDAMENTAL INCOMPLETO'))
+
+#obtém número total de eleitores por municipio para cada categoria
+total_superior = df_eleitores_superior_completo.groupby('cod_municipio_tse').sum('qtd_eleitores_no_perfil').withColumnRenamed('sum(qtd_eleitores_no_perfil)', 'total_sup')
+total_baixa_esc = df_eleitores_baixa_esc.groupby('cod_municipio_tse').sum('qtd_eleitores_no_perfil').withColumnRenamed('sum(qtd_eleitores_no_perfil)', 'total_baixa_esc')
+
+total_rel = total_eleitores.join(total_superior,['cod_municipio_tse'],'inner')
+total_rel = total_rel.join(total_baixa_esc,['cod_municipio_tse'],'inner')
+
+#valor relativo para cada categoria
+rel = total_rel.withColumn('sup_rel',total_rel['total_sup']/total_rel['total'])
+rel = rel.withColumn('baixa_rel',total_rel['total_baixa_esc']/total_rel['total'])
+
+
+#------------------------------------------
+#RESULTADOS
+#conexão com a collection
+spark_resultados = spark \
+    .builder \
+    .appName("resultadosApp") \
+    .config("spark.mongodb.input.uri", "mongodb://10.7.40.54/eleicoes.resultados2014") \
+    .config("spark.mongodb.output.uri", "mongodb://10.7.40.54/eleicoes.resultados2014") \
+    .getOrCreate()
+
+df_resultados = spark_resultados.read.format("com.mongodb.spark.sql.DefaultSource").load()
+
+#seleciona informações úteis
+df_informacoes_resultados = df_resultados.select('codigo_municipio','nome_municipio','sq_candidato','total_votos')
+
+#------------------------------------------
+#CANDIDATOS
+#conexão com a collection
+spark_candidatos = spark \
+    .builder \
+    .appName("candidatosApp") \
+    .config("spark.mongodb.input.uri", "mongodb://10.7.40.54/eleicoes.candidatos2014") \
+    .config("spark.mongodb.output.uri", "mongodb://10.7.40.54/eleicoes.candidatos2014") \
+    .getOrCreate()
+
+df_candidatos = spark_candidatos.read.format("com.mongodb.spark.sql.DefaultSource").load()
+
+#seleciona informações úteis
+df_informacoes_candidatos = df_candidatos.select('sequencial_candidato','sigla_partido','nome_partido')
+
+#juncao das tabelas de resultados e candidatos
+df_res_can = df_informacoes_resultados.join(df_informacoes_candidatos, \
+df_informacoes_resultados['sq_candidato']==df_informacoes_candidatos['sequencial_candidato'],\
+'inner')
+#partido mais votado de cada municipio
+df_partido_municipio = df_res_can.groupBy('nome_municipio','codigo_municipio','sigla_partido').sum('total_votos').withColumnRenamed('sum(total_votos)', 'total_votos').orderBy('total_votos',ascending=False)
+
+#obtém tabela geral com todas as informações necessárias
+df_geral = df_partido_municipio.join(rel,rel['cod_municipio_tse']== df_partido_municipio['codigo_municipio'],'inner')
+
+#------------------------------------------
+#RESPOSTAS
+# Calcula a resposta (valores percentuais dos eleitores).
+res_sup = df_geral.select('nome_municipio','sigla_partido','total_votos','sup_rel').orderBy('sup_rel','total_votos', ascending=False).dropDuplicates(['nome_municipio'])
+res_baixa_esc = df_geral.select('nome_municipio','sigla_partido','total_votos','sup_rel').orderBy('baixa_rel','total_votos', ascending=False).dropDuplicates(['nome_municipio'])
+
+# Armazena o resultado no HDFS.
+res_sup.write.format("csv").save("hdfs://mcruz-master:9000/user/engdados/pergunta3/res_sup.csv")
+res_baixa_esc.format("csv").save("hdfs://mcruz-master:9000/user/engdados/pergunta3/res_baixa_esc.csv")
+print('Encerrado com sucesso')
