ad319f71c8d266003ccd456d83aa7bebf933b5cc
Mateus Targino <mateustargino95@gmail.com>
2018-06-25 09:44:22 -0300

    script para medir tempo de execucao dos programas no spark

--- mongo/medir2.sh
+++ mongo/medir2.sh
@@ -2,7 +2,7 @@
 
 # Comando a ser executado
 
-COMMAND="python data-read-pib.py"
+COMMAND="spark-submit --deploy-mode client --packages org.mongodb.spark:mongo-spark-connector_2.11:2.2.0 pergunta1/pergunta1.py"
 
 # Mede o tempo de execução em milissegundos.
 
@@ -17,4 +17,61 @@ ELAPSED_TIME=$(($END_TIME - $START_TIME))
 ELAPSED_TIME=$(( ELAPSED_TIME/1000000 ))
 
 # Salva a resposta.
-echo "Tempo total do comando '$COMMAND': $ELAPSED_TIME ms." >> ./resultado-medicao.txt
+echo "Tempo total do comando '$COMMAND': $ELAPSED_TIME ms." >> ./resultado-medicao-SPARK.txt
+
+# Comando a ser executado
+
+COMMAND="spark-submit --deploy-mode client --packages org.mongodb.spark:mongo-spark-connector_2.11:2.2.0 pergunta2/pergunta2.py"
+
+# Mede o tempo de execução em milissegundos.
+
+START_TIME=$(date +%s%N)
+
+eval $COMMAND
+
+END_TIME=$(date +%s%N)
+ELAPSED_TIME=$(($END_TIME - $START_TIME))
+
+# Converte para milissegundos.
+ELAPSED_TIME=$(( ELAPSED_TIME/1000000 ))
+
+# Salva a resposta.
+echo "Tempo total do comando '$COMMAND': $ELAPSED_TIME ms." >> ./resultado-medicao-SPARK.txt
+
+# Comando a ser executado
+
+COMMAND="spark-submit --deploy-mode client --packages org.mongodb.spark:mongo-spark-connector_2.11:2.2.0 pergunta3/pergunta3.py"
+
+# Mede o tempo de execução em milissegundos.
+
+START_TIME=$(date +%s%N)
+
+eval $COMMAND
+
+END_TIME=$(date +%s%N)
+ELAPSED_TIME=$(($END_TIME - $START_TIME))
+
+# Converte para milissegundos.
+ELAPSED_TIME=$(( ELAPSED_TIME/1000000 ))
+
+# Salva a resposta.
+echo "Tempo total do comando '$COMMAND': $ELAPSED_TIME ms." >> ./resultado-medicao-SPARK.txt
+
+# Comando a ser executado
+
+COMMAND="spark-submit --deploy-mode client --packages org.mongodb.spark:mongo-spark-connector_2.11:2.2.0 pergunta4/pergunta4.py"
+
+# Mede o tempo de execução em milissegundos.
+
+START_TIME=$(date +%s%N)
+
+eval $COMMAND
+
+END_TIME=$(date +%s%N)
+ELAPSED_TIME=$(($END_TIME - $START_TIME))
+
+# Converte para milissegundos.
+ELAPSED_TIME=$(( ELAPSED_TIME/1000000 ))
+
+# Salva a resposta.
+echo "Tempo total do comando '$COMMAND': $ELAPSED_TIME ms." >> ./resultado-medicao-SPARK.txt
---
+++ mongo/medir2.sh~
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Comando a ser executado
+
+COMMAND="python data-read-pib.py"
+
+# Mede o tempo de execução em milissegundos.
+
+START_TIME=$(date +%s%N)
+
+eval $COMMAND
+
+END_TIME=$(date +%s%N)
+ELAPSED_TIME=$(($END_TIME - $START_TIME))
+
+# Converte para milissegundos.
+ELAPSED_TIME=$(( ELAPSED_TIME/1000000 ))
+
+# Salva a resposta.
+echo "Tempo total do comando '$COMMAND': $ELAPSED_TIME ms." >> ./resultado-medicao.txt
